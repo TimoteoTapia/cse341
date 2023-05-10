56a83093f0bff8db1e833ef8572f92859b282110
Timoteo Tapia <fidel_tapia@byui.edu>
2023-05-09 21:58:45 -0600

    ls-4 swagger finished

--- contact_api/app.js
+++ contact_api/app.js
@@ -8,7 +8,13 @@ const app = express();
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
+    //line 11-17 is needed to give authorization to be desploy in other frontend works
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/', require('./routes'));
